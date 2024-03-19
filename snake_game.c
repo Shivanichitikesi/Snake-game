a1807ceaf539328ebd4a107b19cacbf453d62ca4
Shivanichitikesi <163519514+Shivanichitikesi@users.noreply.github.com>
2024-03-19 23:10:08 +0530

    Add files via upload

--- snake_game.c
+++ snake_game.c
@@ -1,3 +1,4 @@
+//creating protype snake game in c language
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
@@ -10,112 +11,99 @@ void logic();
 int i,j,fruitx=5,fruity=5,k=0,p=10,q=10,c,s;
 int main()
 {
-    
+       //for choosing whether to play or not
         printf("WANNA PLAY 1 or 0\n");
         scanf("%d",&s);
+        //if user wants to play
         if(s==1)
-        {
+        {   
+            //for infinite loop
               while (1)
               {
                 draw();
                 input();
                 logic();
-                sleep(2);
+                sleep(2);//to deplay while loop by 2 seconds
                 system("cls");
               }
         }
         else
         exit(0);
 }
-void draw()
+void draw()//to print the game
 {
-    for(i=0;i<=20;i++)
+    for(i=0;i<=20;i++) //for columns
     {
-        for(j=0;j<=20;j++)
+        for(j=0;j<=20;j++)//for rows
         {
-            if(i==0||j==0||j==20||i==20)
+            if(i==0||j==0||j==20||i==20)//for printing boundary
             {
                 printf("#");
             }
             else if(i==fruitx && j==fruity)
             {
-                printf("*");
+                printf("*"); //for printing fruit aka * symbol 
             }
-            else if(i==p && j==q)
+            else if(i==p && j==q) 
             {
-               // for(int l=1;l<=k/30+1;l++)
-                //{
-                printf("0");
-                /*if (l>1)
-                {
-                    j++;
-                }
-                }*/
+                printf("0"); //for printing snake aka 0 symbol 
             }
 
             else
-            printf(" ");
+            printf(" ");// for background
         }
-        printf("\n");
+        printf("\n");// for next line
     }
-    printf("enter e to exit\n");
-    printf("score:%d",k);
-    //printf("%d",k/30);
-    printf("\nenter your choice\n");
+    printf("enter e to exit\n");//command for exit
+    printf("score:%d",k); //for printing score
+    printf("\nenter your choice\n"); //for printing to ask user choice of direction
 }
-void setup()
+void setup() //for generating a fruit 
 {
-    srand(time(0));
+    srand(time(0)); //for generating random value 
     fruitx=rand()%19+1;
     fruity=rand()%19+1;
-   // printf("%d %d",fruitx,fruity);
-   //return 0;
-
 }
-void input()
+void input() //for accepting value
 {
     char a,w,x,d;
-    if(kbhit())
+    if(kbhit()) //if keyboard hit
     {
-    //scanf("%c",&c);
-    switch(getch())
+    switch(getch())//for accepting character
     {
-    case 'w':
+    case 'w': // for moving upwards direction
         p--;
         break;
-    case 'd':
+    case 'd':// for moving rightside
         q++;
         break;
-    case 'a':
+    case 'a': //for moving leftside
         q--;
         break;
-    case 'x':
+    case 'x': //for moving downwards direction
         p++;
         break;
-    case 'e':
+    case 'e': //for exiting from game
          exit(0);
          break;
     default:
-        printf("enter valid choice\n");
+        printf("enter valid choice\n");// other than w,a,d,x  are invalid choices
         break;
     }
     }
-    //sleep(0.01);
-   // return 0;
 }
 void logic()
 {
-    sleep(1);
-    if(p==fruitx&&q==fruity)
+    sleep(1);//for delaying for 1 seconds
+    if(p==fruitx&&q==fruity) //for checking whether * hitting 0 or not
     {
-       k+=10;
-       setup();
+       k+=10;//to increase the score
+       setup();//for generating another fruit
     }
-    else if(p==0||q==0|p==20||q==20)
+    else if(p==0||q==0|p==20||q==20) //for checking whether 0 hit the boundary
     {
-        printf("GAME OVER\n");
-        printf("SCORE:%d",k);
-        exit(0);
+        printf("GAME OVER\n"); //for printing after the condition is true
+        printf("SCORE:%d",k); //for displaying the score
+        exit(0); //for exiting from the game 
     }
-   // return 0;
 }
